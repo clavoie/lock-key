cda7c49f672d0fd16e189f0c3433c3d0e8592ce0
Chris <clavoie@website.com>
2014-01-06 17:36:54 -0500

    initial commit of project layout

--- README.md
+++ README.md
@@ -2,3 +2,26 @@ lock-key
 ========
 
 Clojure wrappers around Java encryption
+
+## Installation
+
+Add the following dependency to your `project.clj` file:
+
+```
+[lock-key "1.0.0"]
+```
+
+## Usage
+
+FIXME
+
+## Documentation
+
+[API](http://clavoie.github.io/lock-key/)
+
+## License
+
+Copyright © 2014 Chris LaVoie
+
+Distributed under the Eclipse Public License, the same as Clojure.
+
---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject lock-key "0.1.0-SNAPSHOT"
+  :description "Clojure wrappers around Java encryption"
+  :url "https://github.com/clavoie/lock-key"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[charset-bytes "1.0.0"]
+                 [org.clojure/clojure "1.5.1"]]
+  :plugins [[codox "0.6.6"]]
+  :codox {:src-dir-uri "http://github.com/clavoie/lock-key/blob/master/"
+          :src-linenum-anchor-prefix "L"})
