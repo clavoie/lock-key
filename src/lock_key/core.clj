0c6af5e5243c95a0b629d9d66a93b9d9ec821fac
Paul Legato <pjlegato@builtonlogic.com>
2014-04-22 16:34:51 -0700

    Implement AES/CBC/PKCS5Padding mode with initialization vector; expand docs; bump version; add type hints to prevent reflection

--- src/lock_key/core.clj
+++ src/lock_key/core.clj
@@ -1,38 +1,76 @@
 (ns lock-key.core
+  "Wrapper around the Java cryptography extensions in javax.crypto.
+
+   Implements symmetric encryption in AES/CBC/PKCS5Padding mode.
+
+   The user calls (encrypt \"plaintext\" \"key\") and gets 
+   "
   (:require
     [charset-bytes.core :refer [utf8-bytes]])
   (:import 
     [javax.crypto Cipher KeyGenerator SecretKey]
-    [javax.crypto.spec SecretKeySpec]
+    [javax.crypto.spec SecretKeySpec IvParameterSpec]
     [java.security SecureRandom]))
 
-(defn- get-raw-key [seed]
+(set! *warn-on-reflection* true)
+
+(defn- get-raw-key [^String seed]
   (let [keygen (KeyGenerator/getInstance "AES")
-        sr (SecureRandom/getInstance "SHA1PRNG")]
-    (.setSeed sr (utf8-bytes seed))
+        sr     (SecureRandom/getInstance "SHA1PRNG")]
+    (.setSeed sr ^bytes (utf8-bytes seed))
     (.init keygen 128 sr)
     (.. keygen generateKey getEncoded)))
 
-(defn- get-cipher [mode seed]
+
+(defn- ^Cipher get-cipher
+  [mode ^String seed ^bytes iv-bytes]
   (let [key-spec (SecretKeySpec. (get-raw-key seed) "AES")
-        cipher (Cipher/getInstance "AES")]
-    (.init cipher mode key-spec)
+        iv-spec  (IvParameterSpec. iv-bytes)
+        cipher   (Cipher/getInstance "AES/CBC/PKCS5Padding")]
+    (.init cipher (int mode) key-spec iv-spec)
     cipher))
 
-(defn encrypt 
-  "Symmetrically encrypts value with key, such that it can be
-   decrypted later with (decrypt). The value and key parameters are
-   expected to be a String. Returns byte[]"
-  [value key]
-  (let [bytes (utf8-bytes value)
-        cipher (get-cipher Cipher/ENCRYPT_MODE key)]
-    (.doFinal cipher bytes)))
 
-(defn decrypt 
+(defn ^bytes encrypt 
+  "Symmetrically encrypts value with key, such that it can be
+   decrypted later with (decrypt). The value may be either a 
+   UTF-8 String or a byte array. Key should be a String. 
+
+   Returns byte[]. The first 16 bytes of the returned value
+   are the initialization vector. The remainder is the encrypted data."
+  [value ^String key]
+  (let [bytes (if (string? value)
+                ^bytes (utf8-bytes value)
+                value)
+        sr (SecureRandom/getInstance "SHA1PRNG")
+
+        ;; Create a random initialization vector:
+        iv-bytes (byte-array 16)
+        _ (.nextBytes sr iv-bytes)
+        
+        cipher (get-cipher Cipher/ENCRYPT_MODE key iv-bytes)]
+    (into-array Byte/TYPE (concat iv-bytes
+                                  (.doFinal cipher bytes)))))
+
+
+(defn ^bytes decrypt 
   "Decrypts a value which has been encrypted via a call to
    (encrypt) using key. The value parameter is expected to be a 
    byte[] and the key parameter is expected to be a String. 
-   Returns byte[]"
-  [value key]
-  (let [cipher (get-cipher Cipher/DECRYPT_MODE key)]
-    (.doFinal cipher value)))
+   Returns byte[].
+
+   The first 16 bytes of the input value is the initialization vector 
+   to use when decrypting. The remainder is the encrypted data."
+  [^bytes value ^String key]
+  (let [[iv-bytes encrypted-data] (split-at 16 value)
+        iv-bytes (into-array Byte/TYPE iv-bytes)
+        encrypted-data (into-array Byte/TYPE encrypted-data)
+        cipher (get-cipher Cipher/DECRYPT_MODE key iv-bytes)]
+    (.doFinal cipher encrypted-data)))
+
+
+(defn bytes-to-string
+  "Utility for cases where the byte array returned by decrypt is to be converted into a String."
+  [^bytes value]
+  (apply str (map char value)))
+
