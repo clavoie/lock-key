254303650aff1680ea70aa842a14ed833953a329
Chris LaVoie <clavoie@draftkings.com>
2015-08-27 21:15:41 -0400

    Revert adding assertions everywhere. Instead go back ot throwing IllegalArgumentExceptions. Deals with issue #4

--- src/lock_key/core.clj
+++ src/lock_key/core.clj
@@ -15,16 +15,20 @@
   Returns byte[]. The first 16 bytes of the returned value are the
   initialization vector. The remainder is the encrypted data.
 
-  value           - (String/byte[]) the value to encrypt. Raises an AssertionError
+  value           - (String/byte[]) the value to encrypt. Throws IllegalArgumentException
                     if value is an invalid type.
-  encryption-key  - (String) the key with which to encrypt value with. Raises an
-                    AssertionError if encryption-key is an invalid type or empty"
+  encryption-key  - (String) the key with which to encrypt value with. Throws
+                    IllegalArgumentException if encryption-key is empty"
   ^bytes
   [value encryption-key]
-  (assert (or (string? value)
-               (private/byte-array? value)) "Argument [value] must be of type String or byte[]")
-  (assert (and (string? encryption-key)
-               (not (empty? encryption-key))) "Argument [encryption-key] must be a string and not empty")
+
+  (if (not (or (string? value)
+               (private/byte-array? value)))
+    (throw (IllegalArgumentException. "Argument [value] must be of type String or byte[]")))
+
+  (if (empty? encryption-key)
+    (throw (IllegalArgumentException. "Argument [encryption-key] must not be empty")))
+
   (let [value-bytes (if (string? value) (utf8-bytes value) value)
         iv-bytes (private/get-iv-bytes)
         cipher (private/get-cipher Cipher/ENCRYPT_MODE encryption-key iv-bytes)]
@@ -37,15 +41,10 @@
   The first 16 bytes of the input value is the initialization vector to use when
   decrypting. The remainder is the encrypted data.
 
-  value          - (byte[]) the value to decrypt. Raises an AssertionError if value
-                   is an invalid type
-  encryption-key - (String) the key with which the value was encrypted. Raises an
-                   AssertionError if encryption-key is an invalid type or empty"
+  value          - (byte[]) the value to decrypt
+  encryption-key - (String) the key with which the value was encrypted"
   ^bytes
   [value encryption-key]
-  (assert (private/byte-array? value) "Argument [value] must be of type byte[]")
-  (assert (and (string? encryption-key)
-               (not (empty? encryption-key))) "Argument [encryption-key] must be a string and not empty")
   (let [[iv-bytes encrypted-data] (split-at 16 value)
         iv-bytes       (into-array Byte/TYPE iv-bytes)
         encrypted-data (into-array Byte/TYPE encrypted-data)
@@ -56,10 +55,8 @@
   "Decryptes a value which has been encrypted via (encrypt) and attempts to
   construct the decrypted value into a string.
 
-  value          - (byte[]) the value to decrypt. Raises an AssertionError if value
-                   is an invalid type
-  encryption-key - (String) the key with which the value was encrypted. Raises an
-                   AssertionError if encryption-key is an invalid type or empty"
+  value          - (byte[]) the value to decrypt
+  encryption-key - (String) the key with which the value was encrypted"
   [value encryption-key]
   (String. (decrypt value encryption-key)))
 
@@ -69,8 +66,7 @@
 
   value          - (String/byte[]) the value to encrypt. Raises an AssertionError if value
                     is an invalid type
-  encryption-key - (String) the key with which to encrypt value with. Raises an
-                   AssertionError if encryption-key is an invalid type or empty"
+  encryption-key - (String) the key with which to encrypt value with."
   [value encryption-key]
   (String. (base64/encode-bytes (encrypt value encryption-key))))
 
@@ -81,5 +77,4 @@
   value          - (String) the value to decrypt encoded as a base64 string
   encryption-key - (String) the key with which the value was encrypted"
   [value encryption-key]
-  (assert (string? value) "Argument [value] must be of type String")
   (decrypt-as-str (base64/decode-bytes (get-bytes value)) encryption-key))
